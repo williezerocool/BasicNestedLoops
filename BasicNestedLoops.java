16fd600fe63662fc406e608b8216d6074b61b44b
Willie Suggs <klbooker09@gmail.com>
2017-05-14 23:25:37 -0500

    Add files via upload

---
+++ BasicNestedLoops.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicnestedloops;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class BasicNestedLoops {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for(int i=0; i<=5; i++){
+           
+            System.out.println();
+            
+            for(int j=0; j<=5; j++){
+            
+                System.out.print("(" + j + ", " + i + ")");
+            }
+           
+        }
+
+    
+    
+    
+    
+    
+    }
+}
